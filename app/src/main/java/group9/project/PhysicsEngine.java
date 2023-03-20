bec7851544bd33367a5b3361db9c4696829b209f
EthanGoetsch <ethangoetschwork@gmail.com>
2023-03-20 11:04:38 +0100

    Update PhysicsEngine.java

--- app/src/main/java/group9/project/PhysicsEngine.java
+++ app/src/main/java/group9/project/PhysicsEngine.java
@@ -6,24 +6,24 @@ public class PhysicsEngine
 {
     public static final double STEP_TIME = 0.1;
 
-    private static ArrayList<IUpdateable> physicsObjectsToUpdate = new ArrayList<>();
+    private static ArrayList<PhysicsObject> physicsObjectsToUpdate = new ArrayList<>();
 
     public PhysicsEngine()
     {
         physicsObjectsToUpdate = new ArrayList<>();
     }
 
-    public static void addPhysicsObjectToUpdate(IUpdateable updateable)
+    public static void addPhysicsObjectToUpdate(PhysicsObject physicsObject)
     {
-        if (!physicsObjectsToUpdate.contains(updateable))
+        if (!physicsObjectsToUpdate.contains(physicsObject))
         {
-            physicsObjectsToUpdate.add(updateable);
+            physicsObjectsToUpdate.add(physicsObject);
         }
     }
 
     public static void updatePhysicsObjects()
     {
-        for (IUpdateable physicsObject : physicsObjectsToUpdate)
+        for (PhysicsObject physicsObject : physicsObjectsToUpdate)
         {
             physicsObject.update();
         }
