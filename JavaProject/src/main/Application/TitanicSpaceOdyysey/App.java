4d176d32d08ae923184b564f1748b54cbecce471
EthanGoetsch <ethangoetschwork@gmail.com>
2023-03-20 09:19:15 +0100

    Beautified Code

--- JavaProject/src/main/Application/TitanicSpaceOdyysey/App.java
+++ JavaProject/src/main/Application/TitanicSpaceOdyysey/App.java
@@ -11,28 +11,34 @@ import java.io.IOException;
 /**
  * JavaFX App
  */
-public class App extends Application {
-
+public class App extends Application
+{
     private static Scene scene;
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException
+    {
         scene = new Scene(loadFXML("primary"), 640, 480);
+
         stage.setScene(scene);
+
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
+    static void setRoot(String fxml) throws IOException
+    {
         scene.setRoot(loadFXML(fxml));
     }
 
-    private static Parent loadFXML(String fxml) throws IOException {
+    private static Parent loadFXML(String fxml) throws IOException
+    {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+
         return fxmlLoader.load();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch();
     }
-
 }
--- JavaProject/src/main/Application/TitanicSpaceOdyysey/SecondaryController.java
+++ JavaProject/src/main/Application/TitanicSpaceOdyysey/SecondaryController.java
@@ -3,10 +3,11 @@ package project;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class SecondaryController {
-
+public class SecondaryController
+{
     @FXML
-    private void switchToPrimary() throws IOException {
+    private void switchToPrimary() throws IOException
+    {
         App.setRoot("primary");
     }
 }
