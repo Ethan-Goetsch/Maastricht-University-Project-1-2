fd3a331245331869817a317bcf515399bb520da6
Nathan Macdonald <nateml.mac@gmail.com>
2023-03-20 11:07:48 +0100

    rocket ship implements apply force

--- app/src/main/java/group9/project/RocketShipObject.java
+++ app/src/main/java/group9/project/RocketShipObject.java
@@ -2,17 +2,23 @@ package group9.project;
 
 public class RocketShipObject extends PhysicsObject
 {
+
     @Override
-    public void start()
-    {
+    public void start() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'start'");
     }
 
     @Override
-    public void update()
-    {
+    public void update() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'update'");
     }
+
+    @Override
+    public void applyForce(Vector3 force) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'applyForce'");
+    }
+    
 }
