b46dca8c9c8ab1c4429d0c1e642effa7714c25b7
Nateml <nateml.mac@gmail.com>
2023-05-18 11:15:52 +0200

    added some comments

--- app/src/main/java/group9/project/UI/hud/IHudDrawable.java
+++ app/src/main/java/group9/project/UI/hud/IHudDrawable.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package group9.project.UI.hud;
 
 import com.jme3.scene.Node;
@@ -12,9 +8,22 @@ import com.jme3.scene.Node;
  */
 public interface IHudDrawable {
     
+    /**
+     * Use this to retrieve the root node of the drawable's internal scene graph.
+     * By attaching the returned node as a child of the application's gui node, the drawable can be made visible on the HUD.
+     * @return the drawables root node
+     */
     public Node getRootNode();
         
+    /**
+     * Handles any update procedures the drawable needs to execute within the game loop.
+     */
     public void update();
     
+    /**
+     * Use this to enable/disable to drawable object.
+     * Classes which implement this method should add/remove the children of the root node whenever the drawable is enabled/disabled.
+     * @param enabled
+     */
     public void setEnabled(boolean enabled);
 }
